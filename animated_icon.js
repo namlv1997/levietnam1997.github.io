67e91ba04fc4f89e4c8472b5bfbfcae69e26b327
levietnam1997 <77480265+levietnam1997@users.noreply.github.com>
2021-01-19 14:02:38 +0700

    update

--- animated_icon.js
+++ animated_icon.js
@@ -32,38 +32,15 @@ $(document).ready(function() {
         left: githubPos.left
       }, 250);
       
-      $('#stack').animate({
+      $('#facebook').animate({
         top: stackPos.top + 10,
         left: stackPos.left - 3
       }, 500);
     }, 750);
     
-    setTimeout(function() {
-      $('#stack').animate({
-        top: stackPos.top,
-        left: stackPos.left
-      }, 250);
-      
-      $('#linkedin').animate({
-        top: linkedinPos.top + 10,
-        left: linkedinPos.left
-      }, 500);
-    }, 1000);
     
     setTimeout(function() {
-      $('#linkedin').animate({
-        top: linkedinPos.top,
-        left: linkedinPos.left
-      }, 250);
-      
-      $('#code').animate({
-        top: codePos.top + 10,
-        left: codePos.left + 3
-      }, 500);
-    }, 1250);
-    
-    setTimeout(function() {
-      $('#code').animate({
+      $('#facebook').animate({
         top: codePos.top,
         left: codePos.left
       }, 250);
