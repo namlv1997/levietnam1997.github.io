2ba6de930f96499425e74a701b53874f7ee26f9c
levietnam1997 <77480265+levietnam1997@users.noreply.github.com>
2021-01-19 13:56:55 +0700

    update

--- animated_icon.js
+++ animated_icon.js
@@ -5,14 +5,12 @@ $(document).ready(function() {
   $(window).load(function() {
     $('i').show();
   
-    var twitterPos = $('#twitter').position();
     var githubPos = $('#github').position();
     var stackPos = $('#stack').position();
     var linkedinPos = $('#linkedin').position();
     var codePos = $('#code').position();
-    var plusPos = $('#plus').position();
-    var mailPos = $('#mail').position();
-    var imgPos = $('.me').position();
+
+    var imgPos = $('.my-avatar').position();
     
     $('i').css({
       position: 'absolute',
@@ -22,24 +20,12 @@ $(document).ready(function() {
     });
     
     setTimeout(function() {
-      $('#twitter').animate({
+      $('#github').animate({
         top: twitterPos.top + 10,
         left: twitterPos.left - 10
       }, 500);
     }, 250);
-    
-    setTimeout(function() {
-      $('#twitter').animate({
-        top: twitterPos.top,
-        left: twitterPos.left
-      }, 250);
-      
-      $('#github').animate({
-        top: githubPos.top + 10,
-        left: githubPos.left - 6
-      }, 500);
-    }, 500);
-    
+
     setTimeout(function() {
       $('#github').animate({
         top: githubPos.top,
@@ -81,30 +67,7 @@ $(document).ready(function() {
         top: codePos.top,
         left: codePos.left
       }, 250);
+    },2000);
       
-      $('#plus').animate({
-        top: plusPos.top + 10,
-        left: plusPos.left + 6
-      }, 500);
-    }, 1500);
-    
-    setTimeout(function() {
-      $('#plus').animate({
-        top: plusPos.top,
-        left: plusPos.left
-      }, 250);
       
-      $('#mail').animate({
-        top: mailPos.top + 10,
-        left: mailPos.left + 10
-      }, 500);
-    }, 1750);
-    
-    setTimeout(function() {
-      $('#mail').animate({
-        top: mailPos.top,
-        left: mailPos.left
-      }, 250);
-    }, 2000);
-    
   })
